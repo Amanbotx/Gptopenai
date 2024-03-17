aeb811c52286656db349aba30495205e9f4316a7
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-03-17 09:22:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,3 +19,7 @@ DB_URI = environ.get("DB_URI", "mongodb+srv://ranjuvishwakarma50:aman@cluster0.j
 DB_NAME = environ.get("DB_NAME", "Cluster0")
 OPENAI_API = environ.get("OPENAI_API", "")
 AI = is_enabled((environ.get("AI","True")), False)
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002028053413')
+SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
+#ask 
+GOOGLE_API_KEY = environ.get('API_KEY', 'AIzaSyASKXFxSjnvHO_k5kibkqf8DOdLjh9G4Hs')
