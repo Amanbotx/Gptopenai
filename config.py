a8645ab2723c5c4c7962c62921f563593ef17bd8
Amanbotx <159861177+Amanbotx@users.noreply.github.com>
2024-04-05 17:52:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,3 +25,10 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 GOOGLE_API_KEY = environ.get('API_KEY', 'AIzaSyASKXFxSjnvHO_k5kibkqf8DOdLjh9G4Hs')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/Amanchatgroup1')
 OPENAI_API_KEY = environ.get('OPENAI_API_KEY', "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")
+#Openai
+AI = is_enabled((environ.get("AI","True")), True)
+OPENAI_API = environ.get("OPENAI_API", " ")
+DEEP_API = environ.get("DEEP_API", "3ac9b077-654f-45c6-a1f0-a04a5ef6b69e")
+GOOGLE_API_KEY = environ.get("GOOGLE_API_KEY", "AIzaSyD214hhYJ-xf8rfaWX044_g1VEBQ0ua55Q")
+AI_LOGS = int(environ.get("AI_LOGS", "-1002028053413")) #GIVE YOUR NEW LOG CHANNEL ID TO STORE MESSAGES THAT THEY SEARCH IN BOT PM.... [ i have added this to keep an eye on the users message, to avoid misuse of Bot ]
+BOT_USERNAME = environ.get("BOT_USERNAME", "Owner_o_Bot")
